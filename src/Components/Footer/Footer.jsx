91c7abfe8b2f4f100ff7558f0c126128fe8e1724
Sakti001 <110046481+Sarvesh0703@users.noreply.github.com>
2024-07-01 11:46:20 +0530

    Changes

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,31 +1,23 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import './footer.css'
 import { MdOutlineTravelExplore } from "react-icons/md";
 import { ImFacebook } from "react-icons/im";
 import { BsTwitter } from "react-icons/bs";
 import { AiFillInstagram } from "react-icons/ai";
 
-import Aos from 'aos'
-import 'aos/dist/aos.css'
-
 const Footer = () => {
-
-  useEffect(() => {
-    Aos.init({ duration: 2000 })
-  }, [])
-
   return (
     <div className="footer">
       <div className="secContainer container grid">
-        <div data-aos="fade-up" data-aos-duration="2000" className="logoDiv">
+        <div className="logoDiv">
 
-          <div data-aos="fade-up" data-aos-duration="2000" className="footerLogo">
+          <div className="footerLogo">
             <a href="#" className='logo flex'>
               <h1 className="flex"><MdOutlineTravelExplore className='icon' /> Dot</h1>
             </a>
           </div>
 
-          <div data-aos="fade-up" data-aos-duration="3000" className="socials flex">
+          <div className="socials flex">
             <ImFacebook className='icon' />
             <BsTwitter className='icon' />
             <AiFillInstagram className='icon' />
@@ -33,7 +25,7 @@ const Footer = () => {
 
         </div>
 
-        <div data-aos="fade-up" data-aos-duration="3000" className="footerLinks">
+        <div className="footerLinks">
           <span className="linkTitle">Information</span>
           <li><a href="#">Home</a></li>
           <li><a href="#">Explore</a></li>
@@ -41,7 +33,7 @@ const Footer = () => {
           <li><a href="#">Blog</a></li>
         </div>
 
-        <div data-aos="fade-up" data-aos-duration="4000" className="footerLinks">
+        <div className="footerLinks">
           <span className="linkTitle">Helpful Links</span>
           <li><a href="#">Destination</a></li>
           <li><a href="#">Support</a></li>
@@ -49,7 +41,7 @@ const Footer = () => {
           <li><a href="#">Privacy</a></li>
         </div>
 
-        <div data-aos="fade-up" data-aos-duration="5000" className="footerLinks">
+        <div className="footerLinks">
           <span className="linkTitle">Contact Us</span>
           <span className="phone">+91 000 000 000</span>
           <span className="email">istraveling@gmail.com</span>
